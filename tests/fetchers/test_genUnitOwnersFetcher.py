7abf820701732f566ffddda92a5cdb7989b0f8e8
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-07 15:05:52 +0530

    changes

---
+++ tests/fetchers/test_genUnitOwnersFetcher.py
@@ -0,0 +1,19 @@
+import unittest
+from src.fetchers.genUnitOwnersFetcher import getOwnersForGenUnitIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestGenUnitOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the ouatges from reporting software
+        """
+        elemIds = [4, 6]
+        ownersDict = getOwnersForGenUnitIds(self.appConfig['conStr'], elemIds)
+        expectedDict = {4: "ATIL-Adani", 6: "ATIL-Adani"}
+        self.assertTrue(ownersDict == expectedDict)
