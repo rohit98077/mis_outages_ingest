7e32b7234856ad2d87a60cfbc0fda21f47abc019
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-07 13:59:15 +0530

    sql formatting

--- sqls/getElementOwnerNames/getOwnersForBus.sql
+++ sqls/getElementOwnerNames/getOwnersForBus.sql
@@ -1,26 +1,19 @@
 -- get owner names for given BUS id
-SELECT
-    bus.id,
+SELECT bus.id,
     bus.bus_name,
     owner_details.owners
-FROM
-    reporting_web_ui_uat.bus bus
+FROM reporting_web_ui_uat.bus bus
     left join REPORTING_WEB_UI_UAT.associate_substation subs on subs.id = bus.fk_substation_id
     LEFT JOIN (
-        SELECT
-            LISTAGG(own.owner_name, ',') WITHIN GROUP(
-                ORDER BY
-                    owner_name
+        SELECT LISTAGG(own.owner_name, ',') WITHIN GROUP(
+                ORDER BY owner_name
             ) AS owners,
             parent_entity_attribute_id AS element_id
-        FROM
-            reporting_web_ui_uat.entity_entity_reln   ent_reln
-            LEFT JOIN reporting_web_ui_uat.owner                own ON own.id = ent_reln.child_entity_attribute_id
-        WHERE
-            ent_reln.child_entity = 'OWNER'
+        FROM reporting_web_ui_uat.entity_entity_reln ent_reln
+            LEFT JOIN reporting_web_ui_uat.owner own ON own.id = ent_reln.child_entity_attribute_id
+        WHERE ent_reln.child_entity = 'OWNER'
             AND ent_reln.parent_entity = 'ASSOCIATE_SUBSTATION'
             AND ent_reln.child_entity_attribute = 'OwnerId'
             AND ent_reln.parent_entity_attribute = 'Owner'
-        GROUP BY
-            parent_entity_attribute_id
+        GROUP BY parent_entity_attribute_id
     ) owner_details ON owner_details.element_id = subs.id
--- sqls/getElementOwnerNames/getOwnersForFsc.sql
+++ sqls/getElementOwnerNames/getOwnersForFsc.sql
@@ -1,27 +1,19 @@
 -- get owner names for given fsc
-SELECT
-    fsc.id,
+SELECT fsc.id,
     fsc.fsc_name,
     owner_details.owners
-FROM
-    reporting_web_ui_uat.fsc fsc
+FROM reporting_web_ui_uat.fsc fsc
     LEFT JOIN (
-        SELECT
-            LISTAGG(own.owner_name, ',') WITHIN GROUP(
-                ORDER BY
-                    owner_name
+        SELECT LISTAGG(own.owner_name, ',') WITHIN GROUP(
+                ORDER BY owner_name
             ) AS owners,
             parent_entity_attribute_id AS element_id
-        FROM
-            reporting_web_ui_uat.entity_entity_reln   ent_reln
-            LEFT JOIN reporting_web_ui_uat.owner                own ON own.id = ent_reln.child_entity_attribute_id
-        WHERE
-            ent_reln.child_entity = 'OWNER'
+        FROM reporting_web_ui_uat.entity_entity_reln ent_reln
+            LEFT JOIN reporting_web_ui_uat.owner own ON own.id = ent_reln.child_entity_attribute_id
+        WHERE ent_reln.child_entity = 'OWNER'
             AND ent_reln.parent_entity = 'FSC'
             AND ent_reln.child_entity_attribute = 'OwnerId'
             AND ent_reln.parent_entity_attribute = 'Owner'
-        GROUP BY
-            parent_entity_attribute_id
+        GROUP BY parent_entity_attribute_id
     ) owner_details ON owner_details.element_id = fsc.id
-WHERE
-    fsc.id = 4
+WHERE fsc.id = 4
