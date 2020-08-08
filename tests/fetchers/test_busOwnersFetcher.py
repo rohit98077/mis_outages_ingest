60dba96607a82c07de2022721f1070d625e50395
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 18:42:44 +0530

    changes

---
+++ tests/fetchers/test_busOwnersFetcher.py
@@ -0,0 +1,22 @@
+import unittest
+from src.fetchers.busOwnersFetcher import getOwnersForBusIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestBusOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        Buses from reporting software
+        """
+        elemIds = [41, 44]
+        ownersDict = getOwnersForBusIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {
+            41: "POWERGRID-WR1 (PGCIL)", 44: "APL-Adani"}
+        self.assertTrue(ownersDict == expectedDict)
