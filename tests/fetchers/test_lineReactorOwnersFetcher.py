df8738838e207f97387b59633cd432170296695c
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 18:50:12 +0530

    line reactor owners fetcher done

---
+++ tests/fetchers/test_lineReactorOwnersFetcher.py
@@ -0,0 +1,22 @@
+import unittest
+from src.fetchers.lineReactorOwnersFetcher import getOwnersForLineReactorIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestLineReactorOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        LineReactors from reporting software
+        """
+        elemIds = [63,65]
+        ownersDict = getOwnersForLineReactorIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {
+            63: "BDTCL-Sterlite", 65: "NTPC"}
+        self.assertTrue(ownersDict == expectedDict)
