f3f8362948bd6516ff85a8f0b7661b36dc7cfb90
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 19:04:57 +0530

    hvdc pole owners fetch done

---
+++ tests/fetchers/test_hvdcPoleOwnersFetcher.py
@@ -0,0 +1,22 @@
+import unittest
+from src.fetchers.hvdcPoleOwnersFetcher import getOwnersForHvdcPoleIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestHvdcPoleOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        HvdcPoles from reporting software
+        """
+        elemIds = [4, 5]
+        ownersDict = getOwnersForHvdcPoleIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {
+            5: "POWERGRID-WR1 (PGCIL)", 4: "ATIL-Adani"}
+        self.assertTrue(ownersDict == expectedDict)
