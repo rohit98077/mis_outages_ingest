b3674ac3aa0743784399a1d21245b1cdf6a3f2c1
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 18:15:28 +0530

    fetch transformer owners done

---
+++ tests/fetchers/test_transformerOwnersFetcher.py
@@ -0,0 +1,21 @@
+import unittest
+from src.fetchers.transformerOwnersFetcher import getOwnersForTransformerIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestTransformerOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        Transformers from reporting software
+        """
+        elemIds = [28, 31]
+        ownersDict = getOwnersForTransformerIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {28: "Maharashtra", 31: "POWERGRID-WR2 (PGCIL)"}
+        self.assertTrue(ownersDict == expectedDict)
