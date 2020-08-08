8c970450ed709683b5fb0d06df17d8fc2f49d861
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 18:21:50 +0530

    bay owners fetcher done

--- src/fetchers/bayOwnersFetcher.py
+++ src/fetchers/bayOwnersFetcher.py
@@ -2,13 +2,13 @@ from typing import List, Dict
 import cx_Oracle
 
 
-def getOwnersForTransformerIds(reportsConnStr: str, ids: List[int]) -> Dict[int, str]:
+def getOwnersForBayIds(reportsConnStr: str, ids: List[int]) -> Dict[int, str]:
     """fetches the owner names for a given list of 
-    Transformer ids
+    Bay ids
 
     Args:
         reportsConnStr (str): connection string to reports database
-        ids (List[int]): list of Transformer ids
+        ids (List[int]): list of Bay ids
 
     Returns:
         Dict[int, str]: keys will be unit Ids, values will be comma separated owner names
@@ -20,9 +20,9 @@ def getOwnersForTransformerIds(reportsConnStr: str, ids: List[int]) -> Dict[int,
     con = cx_Oracle.connect(reportsConnStr)
 
     # sql to fetch element owners
-    fetchSql = '''SELECT transformer.id,
+    fetchSql = '''SELECT bay.id,
                     owner_details.owners
-                FROM transformer transformer
+                FROM bay bay
                     LEFT JOIN (
                         SELECT LISTAGG(own.owner_name, ',') WITHIN GROUP(
                                 ORDER BY owner_name
@@ -31,12 +31,12 @@ def getOwnersForTransformerIds(reportsConnStr: str, ids: List[int]) -> Dict[int,
                         FROM entity_entity_reln ent_reln
                             LEFT JOIN owner own ON own.id = ent_reln.child_entity_attribute_id
                         WHERE ent_reln.child_entity = 'OWNER'
-                            AND ent_reln.parent_entity = 'TRANSFORMER'
+                            AND ent_reln.parent_entity = 'BAY'
                             AND ent_reln.child_entity_attribute = 'OwnerId'
                             AND ent_reln.parent_entity_attribute = 'Owner'
                         GROUP BY parent_entity_attribute_id
-                    ) owner_details ON owner_details.element_id = transformer.id
-                where transformer.id in ({0})
+                    ) owner_details ON owner_details.element_id = bay.id
+                where bay.id in ({0})
     '''.format(reqIdsTxt)
     # get cursor for querying
     cur = con.cursor()
---
+++ tests/fetchers/test_bayOwnersFetcher.py
@@ -0,0 +1,21 @@
+import unittest
+from src.fetchers.bayOwnersFetcher import getOwnersForBayIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestBayOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        Bays from reporting software
+        """
+        elemIds = [35, 36]
+        ownersDict = getOwnersForBayIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {35: "BDTCL-Sterlite", 36: "POWERGRID-WR2 (PGCIL)"}
+        self.assertTrue(ownersDict == expectedDict)
