ced82c9a61031785e93df964c9698ebb9f824687
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-08 18:58:43 +0530

    bus reactor owners fetcher done

---
+++ tests/fetchers/test_busReactorOwnersFetcher.py
@@ -0,0 +1,22 @@
+import unittest
+from src.fetchers.busReactorOwnersFetcher import getOwnersForBusReactorIds
+import datetime as dt
+from src.appConfig import getConfig
+
+
+class TestBusReactorOwnersFetcher(unittest.TestCase):
+    appConfig: dict = {}
+
+    def setUp(self):
+        self.appConfig = getConfig()
+
+    def test_run(self) -> None:
+        """tests the function that fetches the owners of 
+        BusReactors from reporting software
+        """
+        elemIds = [20, 21]
+        ownersDict = getOwnersForBusReactorIds(
+            self.appConfig['conStr'], elemIds)
+        expectedDict = {
+            20: "POWERGRID-WR2 (PGCIL)", 21: "MEGPTCL"}
+        self.assertTrue(ownersDict == expectedDict)
