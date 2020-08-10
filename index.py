cb429ee5e54da901b667df01d7613249deff2d0d
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-10 15:10:31 +0530

    entry file for raw data ingestion

--- index.py
+++ index.py
@@ -9,3 +9,29 @@ This script creates the data mart for outages data in weekly report
 auto increment in oracle - https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-oracle/
 install Python Docstring Generator for auto documentation of function
 '''
+import argparse
+import datetime as dt
+from src.appConfig import getConfig
+from src.rawDataCreators.outagesRawDataCreator import createOutageEventsRawData
+
+# get start and end dates from command line
+startDate = dt.datetime.now()
+endDate = startDate - dt.timedelta(days=8)
+# get an instance of argument parser from argparse module
+parser = argparse.ArgumentParser()
+# setup firstname, lastname arguements
+parser.add_argument('--start_date', help="Enter Start date in yyyy-mm-dd format",
+                    default=dt.datetime.strftime(startDate, '%Y-%m-%d'))
+parser.add_argument('--end_date', help="Enter last date in yyyy-mm-dd format",
+                    default=dt.datetime.strftime(endDate, '%Y-%m-%d'))
+# get the dictionary of command line inputs entered by the user
+args = parser.parse_args()
+# access each command line input from the dictionary
+startDate = dt.datetime.strptime(args.start_date, '%Y-%m-%d')
+endDate = dt.datetime.strptime(args.end_date, '%Y-%m-%d')
+
+# get application config
+appConfig = getConfig()
+
+# create outages raw data between start and end dates
+_ = createOutageEventsRawData(appConfig, startDate, endDate)
