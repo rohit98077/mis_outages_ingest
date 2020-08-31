54dcc42e2ce0622ac09e488b4831f385084a2268
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-31 16:26:44 +0530

    api service for raw outages creation done

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 '''
 This is the web server that acts as a service that creates outages raw data
 '''
-import argparse
 import datetime as dt
 from src.appConfig import getConfig
 from src.rawDataCreators.outagesRawDataCreator import createOutageEventsRawData
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -19,6 +18,24 @@ app.secret_key = appConfig['flaskSecret']
 @app.route('/')
 def hello():
     return "This is the web server that acts as a service that creates outages raw data"
+
+
+@app.route('/raw_outages', methods=['POST'])
+def create_raw_outages():
+    # get start and end dates from post request body
+    reqData = request.get_json()
+    try:
+        startDate = dt.datetime.strptime(reqData['startDate'], '%Y-%m-%d')
+        endDate = dt.datetime.strptime(reqData['endDate'], '%Y-%m-%d')
+    except Exception as ex:
+        return jsonify({'message': 'Unable to parse start and end dates of this request body'}), 400
+    # create outages raw data between start and end dates
+    isRawDataCreationSuccess = createOutageEventsRawData(
+        appConfig, startDate, endDate)
+    if isRawDataCreationSuccess:
+        return jsonify({'message': 'raw data creation successful!!!', 'startDate': startDate, 'endDate': endDate})
+    else:
+        return jsonify({'message': 'raw data creation was not success'}), 400
 
 
 if __name__ == '__main__':
